ffe8d83e01f62705b361c9d5136f13f158844495
Vladimir Vinakheras <vvinageras@gmail.com>
2024-01-20 02:46:50 +0300

    Initial Commit

---
+++ Vinyl Record Player/Vinyl_Record_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vinyl_Record_PlayerApp.swift
+//  Vinyl Record Player
+//
+//  Created by Vladimir Vinakheras on 20.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Vinyl_Record_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
